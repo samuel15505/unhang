190e4a49c267d33f2183a72c1de6e5f08190f7d6
samuel15505 <sbanwell@free.fr>
2024-03-22 14:01:25 +0100

    cli work

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
     clippy::pedantic,
     clippy::perf,
     clippy::style,
-    clippy::suspicious,
+    clippy::suspicious
 )]
 
 //! A hangman solver. Simple as.
@@ -15,22 +15,41 @@
 //! -u, --update                Update the language file.
 //! -h, --help                  Print help.
 
+use clap::builder::Str;
 use clap::Parser;
+use std::error::Error;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    format: String,
-    language: Option<String>,
-    update: Option<bool>,
+    pos_format: Option<String>,
+    #[arg(short, long, default_value = "english")]
+    language: String,
+    #[arg(short, long)]
+    update: bool,
 }
 
 fn get_words_of_len<'a>(length: usize, words: &[&'a str]) -> Vec<&'a str> {
-    words.iter().filter(|&&word| word.len() == length).copied().collect()
+    words
+        .iter()
+        .filter(|&&word| word.len() == length)
+        .copied()
+        .collect()
 }
 
-fn main() {
+const VALID_CHARS: [char; 11] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'];
+fn format_to_vec(s: &str) -> Result<Vec<u8>, String> {
+     if s.chars().all(|c| VALID_CHARS.contains(&c)) {
+         Ok(s.split('-').map(|c| c.parse().unwrap()).collect())
+     } else { 
+         Err("Invalid character in string".to_string())
+     }
+}
+
+fn main() -> Result<(), &'static str> {
     let args = Args::parse();
-    
+
     println!("{args:?}");
+
+    Ok(())
 }
